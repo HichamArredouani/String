e3b062fb49b62cb731398bc222ae2fd02aad15af
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-09 17:41:08 +0200

    derdeOef

---
+++ src/derdeOef.java
@@ -0,0 +1,8 @@
+public class derdeOef {
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10; i++)
+            sb.append(i).append(" ");
+        System.out.println(sb);
+    }
+}
