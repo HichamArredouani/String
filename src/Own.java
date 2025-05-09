3277505b9072b531a6ec8992d1a1ced97fed34dd
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-09 15:47:12 +0200

    Own

---
+++ src/Own.java
@@ -0,0 +1,7 @@
+public class Own {
+    public static void main(String[] args) {
+        String quote = "Innamal ilmu bi ta'alum";
+        System.out.println(quote.toLowerCase());
+        System.out.println("Character: " + quote.length());
+    }
+}
