23ee2433cf1539be6e3716ec4e9a5fa7a400fbcc
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-09 17:18:16 +0200

    StringBuilder

---
+++ src/Ownn.java
@@ -0,0 +1,10 @@
+public class Ownn {
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder("Learning Java");
+        sb.replace(9, 13, "StringBuilder");
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+
+    }
+}
